c814880446ba6c6923cd7ba9fea8f4990552f145
seif1125 <seifammar1125@gmail.com>
2023-06-10 15:34:49 +0300

    add challenge 1

---
+++ index.js
@@ -0,0 +1,24 @@
+ //Create a function that takes a number as an argument. 
+// Add up all the numbers from 1 to the number you passed to the function. 
+// For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+
+// Examples
+// addUp(4) ➞ 10
+
+// addUp(13) ➞ 91
+
+// addUp(600) ➞ 180300
+
+
+
+function addUp(num) {
+    let numberArray = []; // prepare an array to make our number into step sequence array
+    let initialValue = 0;// set our initial value for summation
+    for (var i = 1; i <= num; i++) {
+      numberArray.push(i); //numbers will be add as [5,4,3,2]etc..
+    }
+    return numberArray.reduce(   //function reduce is a function applied on array which make accumulative function
+      (accumulator, currentvalue) => accumulator + currentvalue,
+      initialValue
+    );
+  }
